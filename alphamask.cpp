4e1dc71ae2a89c946c3b7f41a28c902273d16c29
wmesser <wmesser@g.clemson.edu>
2019-09-30 14:00:03 -0400

    Start alphamask

---
+++ alphamask.cpp
@@ -0,0 +1,10 @@
+#include "Helper.h"
+
+int main (int argc, char * argv[]) {
+  Image img = Image();
+  if (argc != 3) {
+    printf("Incorrect usage.  Correct usage is ./alphamask input.img output.img\n");
+    exit(1);
+  }
+  img = Image(argv[1]);
+}
